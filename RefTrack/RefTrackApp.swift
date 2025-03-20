c976258bc94bb616434d4ae5a6c786840c389ce8
Hajecek <haj8cek@gmail.com>
2025-03-20 15:44:10 +0100

    Initial Commit

---
+++ RefTrack/RefTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefTrackApp.swift
+//  RefTrack
+//
+//  Created by Michal Hájek on 20.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct RefTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RefTrackTests/RefTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  RefTrackTests.swift
+//  RefTrackTests
+//
+//  Created by Michal Hájek on 20.03.2025.
+//
+
+import Testing
+@testable import RefTrack
+
+struct RefTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
